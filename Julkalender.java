706fb8bfbac7faf97bdc2b8304069a562f22be3f
jk <jk>
2015-12-03 16:50:26 +0100

    Made it more OOP

--- Julkalender.java
+++ Julkalender.java
@@ -1,5 +1,4 @@
 import java.time.LocalDate;
-import java.util.Scanner;
 /**
  * Julkalender
  **/
@@ -78,31 +77,5 @@ public class Julkalender
             }
           return out;
         }
-public static void main(String[] args) {
 
-  Julkalender MinJulkalender = new Julkalender();
-
-  Scanner keyboard = new Scanner(System. in );
-  System.out.println("1. Visa dagens lucka 2. Öppna valfri lucka 3. Visa alla öppnade");
-  String input = keyboard.next().trim();
-
-  switch (input) {
-            case "1":  input = "1";
-                    System.out.println("I dagens lucka hittar du:"+MinJulkalender.openToday());
-                     break;
-            case "2":  input = "2";
-                    System.out.println("Vilket datum (En siffra mellan 1 och 24) ?");
-                    int num = keyboard.nextInt();
-                    System.out.println("Där hittar du:"+MinJulkalender.openAny(num));
-                     break;
-            case "3":  input = "3";
-                    System.out.println("Det här har du fått hittills av din Julkalender:"+MinJulkalender.showAllOpen());
-                     break;
-            default: input = "Jag förstår bara 1,2,3 inget annat!";
-                    System.out.println(input);
-                     break;
-        }
-
-
-}
 }
